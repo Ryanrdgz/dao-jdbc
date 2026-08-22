7c2136795228049b9a27512d3af4f2b36e46ee3a
Ryan Rodrigues <ryangato556@gmail.com>
2026-08-22 19:18:54 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entitiel.Department;
+import model.entitiel.Seller;
 
 public class Program {
 
@@ -8,7 +11,10 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
